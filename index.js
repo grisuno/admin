d290892558286823acbe361e56c53f11ecf6e00a
Grisuno <grisuno@gmail.com>
2023-06-16 00:29:59 -0400

    second commit1

--- index.js
+++ index.js
@@ -3,24 +3,10 @@ const path = require('path');
 const sqlite3 = require('sqlite3').verbose();
 const bodyParser = require('body-parser');
 const session = require('express-session');
-const { Pool } = require('pg');
-const url = require('url');
-
 const app = express();
 const port = 3000;
-
-const connectionString = 'postgres://grisisback:Vl380Bp8s9cDuJiVPfCjktVtufKcRUd1@dpg-ci5tnf6nqql3q3f13al0-a.ohio-postgres.render.com/grisdb';
-const params = url.parse(connectionString);
-const auth = params.auth.split(':');
-
-const config = {
-  user: auth[0],
-  password: auth[1],
-  host: params.hostname,
-  port: params.port,
-  database: params.pathname.split('/')[1],
-};
-
+const { Pool } = require('pg');
+const url = require('url');
 
 // Configure session middleware
 app.use(session({
@@ -30,11 +16,11 @@ app.use(session({
 }));
 
 // Configure SQLite database
-//const db = new sqlite3.Database('web');
-const pool = new Pool(config);
+const db = new sqlite3.Database('web');
 
 // Create table for contact form data
-pool.query(`
+db.serialize(() => {
+  db.run(`
     CREATE TABLE IF NOT EXISTS contacts (
       id INTEGER PRIMARY KEY AUTOINCREMENT,
       name TEXT,
@@ -44,20 +30,12 @@ pool.query(`
       updatedAt TEXT,
       status INTEGER
     )
-  `, (err, result) => {
-    if (err) {
-      console.error('Error al ejecutar la consulta', err);
-
-    } else {
-      const users = result.rows; // Obtén los resultados de la consulta
-
-      // Hacer algo con los datos (por ejemplo, enviarlos como respuesta JSON)
-     
-    }
+  `);
 });
 
 // Create table for user data
-pool.query(`
+db.serialize(() => {
+  db.run(`
     CREATE TABLE IF NOT EXISTS users (
       id INTEGER PRIMARY KEY AUTOINCREMENT,
       name TEXT,
@@ -65,16 +43,7 @@ pool.query(`
       username TEXT,
       password TEXT
     )
-  `, (err, result) => {
-    if (err) {
-      console.error('Error al ejecutar la consulta', err);
-   
-    } else {
-      const users = result.rows; // Obtén los resultados de la consulta
-
-      // Hacer algo con los datos (por ejemplo, enviarlos como respuesta JSON)
-
-    }
+  `);
 });
 
 // Configure body-parser middleware
@@ -231,51 +200,46 @@ app.get('/tables-data.html', (req, res) => {
   if (req.session.isLoggedIn) {
     // El usuario ha iniciado sesión, puedes acceder a req.session.username para obtener el nombre de usuario
     const username = req.session.username;
-    
-    // Ejecutar la consulta SELECT en PostgreSQL
-    pool.query('SELECT * FROM users', (err, result) => {
-      if (err) {
-        console.error('Error al obtener los usuarios', err);
-        res.sendStatus(500);
-      } else {
-        const users = result.rows; // Obtén los resultados de la consulta
-
-        // Renderizar la página 'tables-data.html' y pasar los datos de usuario como variable
-        res.render('tables-data.html', { username, users });
-      }
-    });
+        // Consultar todos los usuarios en la tabla 'users'
+        db.all('SELECT * FROM users', (err, users) => {
+          if (err) {
+            console.error('Error al obtener los usuarios', err);
+            res.sendStatus(500);
+          } else {
+            // Devolver los datos como JSON y pasarlos al render
+            res.render('tables-data.html', { username, users });
+          }
+        });
   } else {
     // El usuario no ha iniciado sesión, redirigir a la página de inicio de sesión
     res.redirect('/pages-login.html');
   }
-});
 
+
+});
 
 app.get('/tables-general.html', (req, res) => {
-  // Verificar si el usuario ha iniciado sesión
-  if (req.session.isLoggedIn) {
-    // El usuario ha iniciado sesión, puedes acceder a req.session.username para obtener el nombre de usuario
-    const username = req.session.username;
-    
-    // Ejecutar la consulta SELECT en PostgreSQL
-    pool.query('SELECT * FROM contacts', (err, result) => {
-      if (err) {
-        console.error('Error al obtener los contactos', err);
-        res.sendStatus(500);
-      } else {
-        const contacts = result.rows; // Obtén los resultados de la consulta
-
-        // Renderizar la página 'tables-general.html' y pasar los datos de usuario como variable
-        res.render('tables-general.html', { username, contacts });
-      }
-    });
-  } else {
-    // El usuario no ha iniciado sesión, redirigir a la página de inicio de sesión
-    res.redirect('/pages-login.html');
-  }
-});
-
+    // Verificar si el usuario ha iniciado sesión
+    if (req.session.isLoggedIn) {
+      // El usuario ha iniciado sesión, puedes acceder a req.session.username para obtener el nombre de usuario
+      const username = req.session.username;
+          // Consultar todos los usuarios en la tabla 'contacts'
+          db.all('SELECT * FROM contacts', (err, contacts) => {
+            if (err) {
+              console.error('Error al obtener los contactos', err);
+              res.sendStatus(500);
+            } else {
+              // Devolver los datos como JSON y pasarlos al render
+              res.render('tables-general.html', { username, contacts });
+            }
+          });
+    } else {
+      // El usuario no ha iniciado sesión, redirigir a la página de inicio de sesión
+      res.redirect('/pages-login.html');
+    }
   
+
+});
 
 app.get('/users-profile.html', (req, res) => {
   // Verificar si el usuario ha iniciado sesión
@@ -294,46 +258,46 @@ app.get('/users-profile.html', (req, res) => {
 // Login route
 app.post('/login', (req, res) => {
   const { username, password } = req.body;
-
-  // Verificar si el nombre de usuario y la contraseña coinciden con un usuario en la base de datos
-  pool.query('SELECT * FROM users WHERE username = $1 AND password = $2', [username, password], (err, result) => {
+  
+  // Check if the username and password match a user in the database
+  db.get('SELECT * FROM users WHERE username = ? AND password = ?', [username, password], (err, row) => {
     if (err) {
-      console.error('Error al obtener el usuario de la base de datos', err);
+      console.error('Error fetching user from database', err);
       res.sendStatus(500);
-    } else if (result.rows.length > 0) {
+    } else if (row) {
       // Usuario encontrado, guardar el estado de inicio de sesión en la sesión
       req.session.isLoggedIn = true;
       req.session.username = username;
-
+      
       // Redirigir a la página de perfil de usuario
       res.redirect('/users-profile.html');
     } else {
-      // Usuario no encontrado o credenciales incorrectas, redirigir a la página de inicio de sesión con un mensaje de error
+      // User not found or incorrect credentials, redirect to the login page with an error message
       res.redirect('/pages-login.html?error=1');
     }
   });
 });
 
-// Ruta de registro
+// Register route
 app.post('/register', (req, res) => {
   const { name, email, username, password } = req.body;
 
-  // Verificar si el nombre de usuario ya existe en la base de datos
-  pool.query('SELECT * FROM users WHERE username = $1', [username], (err, result) => {
+  // Check if the username already exists in the database
+  db.get('SELECT * FROM users WHERE username = ?', [username], (err, row) => {
     if (err) {
-      console.error('Error al obtener el usuario de la base de datos', err);
+      console.error('Error fetching user from database', err);
       res.sendStatus(500);
-    } else if (result.rows.length > 0) {
-      // Usuario con el mismo nombre de usuario ya existe, redirigir a la página de registro con un mensaje de error
+    } else if (row) {
+      // User with the same username already exists, redirect to the registration page with an error message
       res.redirect('/pages-register.html?error=1');
     } else {
-      // Insertar el nuevo usuario en la base de datos
-      pool.query('INSERT INTO users (name, email, username, password) VALUES ($1, $2, $3, $4)', [name, email, username, password], (err) => {
+      // Insert the new user into the database
+      db.run('INSERT INTO users (name, email, username, password) VALUES (?, ?, ?, ?)', [name, email, username, password], function(err) {
         if (err) {
-          console.error('Error al insertar el usuario en la base de datos', err);
+          console.error('Error inserting user into database', err);
           res.sendStatus(500);
         } else {
-          // Nuevo usuario registrado con éxito, redirigir a la página de inicio de sesión
+          // New user successfully registered, redirect to the login page
           res.redirect('/pages-login.html');
         }
       });
@@ -341,30 +305,33 @@ app.post('/register', (req, res) => {
   });
 });
 
-// Ruta del formulario de contacto
+
+// Contact form route
 app.post('/api/contact', (req, res) => {
-  const { name, email, subject, message } = req.body;
+  const { name, email,subject, message } = req.body;
   const createdAt = new Date().toISOString();
   const updatedAt = new Date().toISOString();
   const status = 1;
 
-  // Insertar los datos del formulario en la base de datos PostgreSQL
-  pool.query(
-    'INSERT INTO contacts (name, email, subject, message, created_at, updated_at, status) VALUES ($1, $2, $3, $4, $5, $6, $7)',
-    [name, email, subject, message, createdAt, updatedAt, status],
-    (err) => {
+  // Insert form data into SQLite database
+  db.run(
+    'INSERT INTO contacts (name, email, message, createdAt, updatedAt, status) VALUES (?, ?, ?, ?, ?, ?)',
+    [name, email, message, createdAt, updatedAt, status],
+    function(err) {
       if (err) {
-        console.error('Error al insertar los datos en la base de datos', err);
+        console.error('Error inserting data into database', err);
         res.sendStatus(500);
       } else {
-        // Recuperar la fila insertada para obtener el ID autoincremental
-        pool.query('SELECT * FROM contacts ORDER BY id DESC LIMIT 1', (err, result) => {
+        // Retrieve the inserted row to get the auto-incremented ID
+        const contactId = this.lastID;
+        // Fetch the inserted contact from the database
+        db.get('SELECT * FROM contacts WHERE id = ?', [contactId], (err, row) => {
           if (err) {
-            console.error('Error al obtener el contacto de la base de datos', err);
+            console.error('Error fetching contact from database', err);
             res.sendStatus(500);
           } else {
-            // Enviar el contacto insertado como respuesta JSON
-            res.json(result.rows[0]);
+            // Send the inserted contact as JSON response
+            res.sendStatus(200);
           }
         });
       }
@@ -372,20 +339,19 @@ app.post('/api/contact', (req, res) => {
   );
 });
 
-// Ruta para obtener todos los contactos
+// Get all contacts route
 app.get('/api/contacts', (req, res) => {
-  // Obtener todos los contactos de la base de datos PostgreSQL
-  pool.query('SELECT * FROM contacts', (err, result) => {
+  // Fetch all contacts from the database
+  db.all('SELECT * FROM contacts', (err, rows) => {
     if (err) {
-      console.error('Error al obtener los contactos de la base de datos', err);
+      console.error('Error fetching contacts from database', err);
       res.sendStatus(500);
     } else {
-      // Enviar los contactos como respuesta JSON
-      res.json(result.rows);
+      // Send the contacts as JSON response
+      res.json(rows);
     }
   });
 });
-
 
 // Handle not found routes
 app.use((req, res) => {
